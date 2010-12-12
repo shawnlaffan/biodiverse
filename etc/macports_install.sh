d4df56ad7d242a2fa70d3d589f19d1c30d8d492f
shawnlaffan@gmail.com <shawnlaffan@users.noreply.github.com>
2010-12-12 04:01:49 +0000

    an update to the mac install script

--- etc/macports_install.sh
+++ etc/macports_install.sh
@@ -7,6 +7,7 @@
 sudo port install p5-gtk2-gladexml
 sudo port install p5-gnome2-canvas
 
+sudo /opt/local/bin/perl -MCPAN -e 'install LWP::Simple'
 sudo /opt/local/bin/perl -MCPAN -e 'install Bundle::BiodiverseNoGUI'
 sudo /opt/local/bin/perl -MCPAN -e 'install Bundle::Biodiverse'
 # repeat this command as one of the libs is not installed on the first go

ac4bf8ee2c07693b20029adca22aeaf04cf819c8
shawnlaffan@gmail.com <shawnlaffan@gmail.com@066c9922-a8b7-11de-ac14-117a7284e870>
2012-01-23 06:11:36 +0000

    more moving bundles to task name space

--- trunk/etc/bundles/GUI/lib/Task/Biodiverse.pm
+++ trunk/etc/bundles/GUI/lib/Task/Biodiverse.pm
@@ -1,10 +1,9 @@
-package Bundle::Biodiverse;
+package Task::Biodiverse;
 
 use strict;
 use warnings;
 
-#use vars qw($VERSION);
-our $VERSION = '0.16';
+our $VERSION = '0.16001';
 
 
 1;
@@ -19,29 +18,15 @@ Bundle::Biodiverse - Bundle to install Biodiverse dependencies.
 =head1 SYNOPSIS
 
   #  on Windows:
-  perl -MCPAN -e 'install Bundle::Biodiverse'
+  perl -MCPAN -e 'install Task::Biodiverse'
   
   #  on most other platforms:
-  sudo perl -MCPAN -e 'install Bundle::Biodiverse'
-
-=head1 CONTENTS
-
-Bundle::BiodiverseNoGUI
-
-Geo::ShapeFile
-
-Tree::R
-
-Gtk2::GladeXML
-
-Browser::Open
-
-LWP
+  sudo perl -MCPAN -e 'install Task::Biodiverse'
 
 
 =head1 DESCRIPTION
 
-CPAN Bundle file for Biodiverse dependencies across all platforms.
+Task to install Biodiverse dependencies across all platforms.
 This does not include the Gtk dependencies as they don't load cleanly on all platforms.
 
 See L<http://www.purl.org/biodiverse> for more details about Biodiverse itself.  
---
+++ trunk/etc/bundles/NoGUI/lib/Task/Biodiverse/NoGUI.pm
@@ -0,0 +1,37 @@
+package Task::Biodiverse::NoGUI;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.16001';
+
+
+1;
+
+__END__
+
+=head1 NAME
+
+Task::Biodiverse::NoGUI - Task to install Biodiverse dependencies for non-GUI use
+
+=head1 SYNOPSIS
+
+  #  on Windows:
+  perl -MCPAN -e 'install Task::Biodiverse::NoGUI'
+  
+  #  on most other platforms:
+  sudo perl -MCPAN -e 'install Task::Biodiverse::NoGUI'
+
+
+=head1 DESCRIPTION
+
+Task file for Biodiverse dependencies for non-GUI use across all platforms.
+
+See Task::Biodiverse for the additional libs needed by the GUI.
+
+See L<http://www.purl.org/biodiverse> for more details about Biodiverse itself.  
+
+
+=head1 AUTHOR
+
+Shawn Laffan

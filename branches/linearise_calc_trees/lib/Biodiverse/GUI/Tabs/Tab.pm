60cefa62ff60489abde4e5f7ec5425fc8d0c274d
shawnlaffan@gmail.com <shawnlaffan@gmail.com@066c9922-a8b7-11de-ac14-117a7284e870>
2012-07-07 10:53:17 +0000

    Branch to trial implementation of a linear dependency tree for indices.

---
+++ branches/linearise_calc_trees/lib/Biodiverse/GUI/Tabs/Tab.pm
@@ -0,0 +1,288 @@
+package Biodiverse::GUI::Tabs::Tab;
+use strict;
+use warnings;
+
+our $VERSION = '0.17';
+
+use Gtk2;
+use Biodiverse::GUI::GUIManager;
+use Biodiverse::GUI::Project;
+use Carp;
+
+
+sub add_to_notebook {
+    my $self = shift;
+    my %args = @_;
+
+    my $page  = $args{page};
+    my $label = $args{label};
+    my $label_widget = $args{label_widget};
+
+    $self->{notebook}   = $self->{gui}->getNotebook();
+    $self->{notebook}->append_page_menu($page, $label, $label_widget);
+    $self->{page}       = $page;
+    $self->{gui}->addTab($self);
+    $self->set_tab_reorderable($page);
+
+    return;
+}
+
+sub getPageIndex {
+    my $self = shift;
+    my $page = shift || $self->{page};
+    my $index = $self->{notebook}->page_num($page);
+    return $index >= 0 ? $index : undef;
+}
+
+sub setPageIndex {
+    my $self = shift;
+    
+    #  no-op now
+    #$self->{page_index} = shift;
+    
+    return;
+}
+
+sub get_base_ref {
+    my $self = shift;
+
+    #  check all possibilities
+    #  should really just have one
+    foreach my $key (qw /base_ref basedata_ref selected_basedata_ref/) {
+        if (exists $self->{$key}) {
+            return $self->{$key};
+        }
+    }
+
+    croak "Unable to access the base ref\n";
+}
+
+sub get_current_registration {
+    my $self = shift;
+    return $self->{current_registration};
+}
+
+sub update_current_registration {
+    my $self = shift;
+    my $object = shift;
+    $self->{current_registration} = $object;
+}
+
+sub update_name {
+    my $self = shift;
+    my $new_name = shift;
+    #$self->{current_registration} = $new_name;
+    eval {$self->{label_widget} -> set_text ($new_name)};
+    eval {$self->{title_widget} -> set_text ($new_name)};
+    eval {$self->{tab_menu_label}->set_text ($new_name)};
+    return;
+}
+
+sub remove {
+    my $self = shift;
+
+    if (exists $self->{current_registration}) {  #  deregister if necessary
+        #$self->{project}->registerInOutputsModel($self->{current_registration}, undef);
+        $self->registerInOutputsModel($self->{current_registration}, undef);
+    }
+    my $index = $self->getPageIndex;
+    if (defined $index && $index > -1) {
+        $self->{notebook}->remove_page( $index );
+    }
+
+    return;
+}
+
+sub set_tab_reorderable {
+    my $self = shift;
+    my $page = shift || $self->{page};
+
+    $self->{notebook}->set_tab_reorderable($page, 1);
+
+    return;
+}
+
+sub onClose {
+    my $self = shift;
+    $self->{gui}->removeTab($self);
+    #print "[GUI] Closed tab - ", $self->getPageIndex(), "\n";
+    return;
+}
+
+# Make ourselves known to the Outputs tab to that it
+# can switch to this tab if the user presses "Show"
+sub registerInOutputsModel {
+    my $self = shift;
+    my $output_ref = shift;
+    my $tabref = shift; # either $self, or undef to deregister
+    my $model = $self->{project}->getBaseDataOutputModel();
+
+    # Find iter
+    my $iter;
+    my $iter_base = $model->get_iter_first();
+
+    while ($iter_base) {
+        
+        my $iter_output = $model->iter_children($iter_base);
+        while ($iter_output) {
+            if ($model->get($iter_output, MODEL_OBJECT) eq $output_ref) {
+                $iter = $iter_output;
+                last; #FIXME: do we have to look at other iter_bases, or does this iterate over entire level?
+            }
+            
+            $iter_output = $model->iter_next($iter_output);
+        }
+        
+        last if $iter; # break if found it
+        $iter_base = $model->iter_next($iter_base);
+    }
+
+    if ($iter) {
+        $model->set($iter, MODEL_TAB, $tabref);
+        $self->{current_registration} = $output_ref;
+    }
+    
+    return;
+}
+
+##########################################################
+# Keyboard shortcuts
+##########################################################
+
+my $snooper_id;
+my $handler_entered = 0;
+
+# Called when user switches to this tab
+#   installs keyboard-shortcut handler
+sub setKeyboardHandler {
+    my $self = shift;
+    # Make CTRL-G activate the "go!" button (onRun)
+    if ($snooper_id) {
+        ##print "[Tab] Removing keyboard snooper $snooper_id\n";
+        Gtk2->key_snooper_remove($snooper_id);
+        $snooper_id = undef;
+    }
+
+
+    $snooper_id = Gtk2->key_snooper_install(\&hotkeyHandler, $self);
+    ##print "[Tab] Installed keyboard snooper $snooper_id\n";
+}
+
+sub removeKeyboardHandler {
+    my $self = shift;
+    if ($snooper_id) {
+        ##print "[Tab] Removing keyboard snooper $snooper_id\n";
+        Gtk2->key_snooper_remove($snooper_id);
+        $snooper_id = undef;
+    }
+}
+    
+# Processes keyboard shortcuts like CTRL-G = Go!
+sub hotkeyHandler {
+    my ($widget, $event, $self) = @_;
+    my $retval;
+
+    # stop recursion into onRun if shortcut triggered during processing
+    #   (this happens because progress-dialogs pump events..)
+
+    return 1 if ($handler_entered == 1);
+
+    $handler_entered = 1;
+
+    if ($event->type eq 'key-press') {
+        # if CTL- key is pressed
+        if ($event->state >= ['control-mask']) {
+            my $keyval = $event->keyval;
+            #print $keyval . "\n";
+            
+            # Go!
+            if ((uc chr $keyval) eq 'G') {
+                $self->onRun();
+                $retval = 1; # stop processing
+            }
+
+            # Close tab (CTRL-W)
+            elsif ((uc chr $keyval) eq 'W') {
+                if ($self->getRemovable) {
+                    $self->{gui}->removeTab($self);
+                    $retval = 1; # stop processing
+                }
+            }
+
+            # Change to next tab
+            elsif ($keyval eq Gtk2::Gdk->keyval_from_name ('Tab')) {
+                #  switch tabs
+                #print "keyval is $keyval (tab), state is " . $event->state . "\n";
+                my $page_index = $self->getPageIndex;
+                $self->{gui}->switchTab (undef, $page_index + 1); #  go right
+            }
+            elsif ($keyval eq Gtk2::Gdk->keyval_from_name ('ISO_Left_Tab')) {
+                #  switch tabs
+                #print "keyval is $keyval (left tab), state is " . $event->state . "\n";
+                my $page_index = $self->getPageIndex;
+                $self->{gui}->switchTab (undef, $page_index - 1); #  go left
+            }
+        }
+    }
+
+    $handler_entered = 0;
+    $retval = 0; # continue processing
+    return $retval;
+}
+
+######################################
+#  Other stuff
+
+
+sub onRun {} # default for tabs that don't implement onRun
+
+sub getRemovable { return 1; } # default - tabs removable
+
+#  codes to define percentiles etc
+sub get_display_stretch_codes {
+    my $self = shift;
+    
+    my %codes = (
+        '2.5'  => 'PCT025',
+        '97.5' => 'PCT975',
+        '5'    => 'PCT05',
+        '95'   => 'PCT95',
+    );
+
+    return wantarray ? %codes : \%codes;
+}
+
+sub get_plot_min_max_values {
+    my $self = shift;
+
+    my @minmax = ($self->{plot_min_value}, $self->{plot_max_value});
+
+    return wantarray ? @minmax : \@minmax;
+}
+
+sub set_legend_ltgt_flags {
+    my $self = shift;
+    my $stats = shift;
+
+    my $flag = 0;
+    my $stat_name = ($self->{PLOT_STAT_MIN} || 'MIN');
+    eval {
+        if ($stats->{$stat_name} != $stats->{MIN}
+            and $stat_name =~ /PCT/) {
+            $flag = 1;
+        }
+        $self->{grid}->set_legend_lt_flag ($flag);
+    };
+    $flag = 0;
+    $stat_name = ($self->{PLOT_STAT_MAX} || 'MAX');
+    eval {
+        if ($stats->{$stat_name} != $stats->{MAX}
+            and $stat_name =~ /PCT/) {
+            $flag = 1;
+        }
+        $self->{grid}->set_legend_gt_flag ($flag);
+    };
+    return;
+}
+
+1;
---
+++ branches/linearise_calc_trees/lib/Biodiverse/TestHelpers.pm
@@ -0,0 +1,143 @@
+#  helper functions for testing
+package Biodiverse::TestHelpers;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.17';
+
+use File::Temp;
+
+use Exporter::Easy (
+    TAGS => [
+        basedata => [
+            qw(
+                get_basedata_import_data_file
+                get_basedata_test_data
+                get_basedata_object
+            ),
+        ],
+        element_properties => [
+            qw (
+                get_element_properties_test_data
+            )
+        ],
+    ],
+);
+
+
+sub get_basedata_import_data_file {
+    my %args = @_;
+
+    my $tmp_obj = File::Temp->new;
+    my $ep_f = $tmp_obj->filename;
+    print $tmp_obj get_basedata_test_data(@_);
+    $tmp_obj -> close;
+
+    return $tmp_obj;
+}
+
+sub get_basedata_test_data {
+    my %args = (
+        x_spacing => 1,
+        y_spacing => 1,
+        x_max     => 100,
+        y_max     => 100,
+        x_min     => 1,
+        y_min     => 1,
+        count     => 1,
+        @_,
+    );
+
+    my $count = $args{count} || 0;
+    my $use_rand_counts = $args{use_rand_counts};
+
+    my $data;
+    $data .= "label,x,y,count\n";
+    foreach my $i ($args{x_min} .. $args{x_max}) {
+        my $ii = $i * $args{x_spacing};
+        foreach my $j ($args{y_min} .. $args{y_max}) {
+            my $jj = $j * $args{y_spacing};
+            if ($use_rand_counts) {
+                $count = int (rand() * 1000);
+            }
+            $data .= "$i"."_$j,$ii,$jj,$count\n";
+        }
+    }
+
+    return $data;
+}
+
+sub get_basedata_object {
+    my %args = @_;
+
+    my $bd_f = get_basedata_import_data_file(@_);
+
+    print "Temp file is $bd_f\n";
+
+    my $bd = Biodiverse::BaseData->new(
+        CELL_SIZES => $args{CELL_SIZES},
+        NAME       => 'Test basedata',
+    );
+    $bd->import_data(
+        input_files   => [$bd_f],
+        group_columns => [1, 2],
+        label_columns => [0],
+        sample_count_columns => [3],
+    );
+    
+    return $bd;
+}
+
+
+sub get_element_properties_test_data {
+
+    my $data = <<'END_DATA'
+rec_num,genus,species,new_genus,new_species,range,sample_count,num
+1,Genus,sp1,Genus,sp2,,1
+10,Genus,sp18,Genus,sp2,,1
+2000,Genus,sp2,,,200,1000,2
+END_DATA
+  ;
+
+}
+
+
+
+__END__
+
+
+=head1 NAME
+
+Biodiverse::TestHelpers - helper functions for Biodiverse tests.
+
+=head1 SYNOPSIS
+
+  use Biodiverse::TestHelpers;
+
+=head1 DESCRIPTION
+
+Helper functions for Biodiverse tests.  Mostly provides data.
+
+=head1 METHODS
+
+=over 4
+
+=item get_element_properties_test_data();
+
+Element properties table data.
+
+=back
+
+=head1 AUTHOR
+
+Shawn Laffan
+
+=head1 LICENSE
+
+LGPL
+
+=head1 SEE ALSO
+
+See http://www.purl.org/biodiverse for more details.
+

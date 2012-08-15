614bd421273085af37bab5df32eaabc2eda61386
shawnlaffan@gmail.com <shawnlaffan@users.noreply.github.com>
2012-08-15 04:56:39 +0000

    Add checks so we don;t use all the available RAM and freeze the OS.

--- bin/experiments/matrix_size.pl
+++ bin/experiments/matrix_size.pl
@@ -11,12 +11,20 @@ use Devel::Size qw /total_size/;
 my $rand = 1;
 srand (23);
 
+sub bytes_to_megabytes {
+    my $val = shift;
+    my $conv = 1024 * 1024;
+    return $val / $conv;
+}
+
+my $use_bmx = 0;
+my @use_rand = (0); #  or (0, 1)
 
 #my $max = 1000;
-foreach my $max (100, 1000, 1500) {
+foreach my $max (100, 1000, 3000, 5000) {
     print "max is $max\n";
-    foreach my $rand (0, 1) {
-        my $bmx = Biodiverse::Matrix->new (NAME => 'checker');
+    foreach my $rand (@use_rand) {
+        my $bmx = $use_bmx && $max <= 1500 ? Biodiverse::Matrix->new (NAME => 'checker') : undef;
         my $mx_hash = {};
         my $valsub = $rand ? sub {return rand()} : sub {return shift};
     
@@ -26,17 +34,20 @@ foreach my $max (100, 1000, 1500) {
                 next if $j < $i;
                 my $value = &$valsub($i);
                 $mx_hash->{$i}{$j} = $i;
-                $bmx->add_element(
-                    element1 => $i,
-                    element2 => $j,
-                    value    => $value);
+                eval {
+                    $bmx->add_element(
+                        element1 => $i,
+                        element2 => $j,
+                        value    => $value,
+                    );
+                };
             }
         }
         print "\tRand is $rand\n";
         my $sz_bmx  = total_size ($bmx);
         my $sz_hash = total_size ($mx_hash);
-        printf "\tB::MX is %10i\n", $sz_bmx;
-        printf "\thash is  %10i\n", $sz_hash;
-        printf "\tratio is %f\n", $sz_bmx / $sz_hash;
+        printf "\tB::MX is %10.3f MiB\n", bytes_to_megabytes $sz_bmx;
+        printf "\thash  is %10.3f MiB\n", bytes_to_megabytes $sz_hash;
+        printf "\tratio is %10.3f\n", $sz_bmx / $sz_hash;
     }
 }

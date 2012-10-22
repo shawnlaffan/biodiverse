0786ea32989f7aecfa16dcc2578489fd58f2d9a2
zhoumichaely@gmail.com <zhoumichaely@gmail.com@066c9922-a8b7-11de-ac14-117a7284e870>
2012-10-22 04:18:32 +0000

    Update issue 340 Working on Strawberry Perl 5.16.1 MSWin32-x86-multi-thread using OpenSUSE GTK builds.

---
+++ etc/win32_gtk/c/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/usr/i686-w64-mingw32/sys-root/mingw/lib"
+XML2_LIBS="-lxml2 -lz    "
+XML2_INCLUDEDIR="-I/usr/i686-w64-mingw32/sys-root/mingw/include/libxml2"
+MODULE_VERSION="xml2-2.8.0"
+

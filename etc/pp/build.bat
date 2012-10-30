7980712dd768d515afdb62a658f718519ddc90af
zhoumichaely@gmail.com <zhoumichaely@gmail.com@066c9922-a8b7-11de-ac14-117a7284e870>
2012-10-30 05:44:08 +0000

    Update issue 340 PAR now builds and runs properly. Still need to handle the actual "native" (as opposed to Perl module) GTK DLLs.

--- etc/pp/build.bat
+++ etc/pp/build.bat
@@ -1,2 +1,6 @@
 rem run from bin directory
-pp -vvv -o BiodiverseGUI.exe BiodiverseGUI.pl -M ../etc/pp/ModUser.pm
+call pp -v -p -B -o BiodiverseGUI.par BiodiverseGUI.pl -x
+rem above line will open Biodiverse. it needs to be closed for script to continue
+rem TODO: maybe add logic to BiodiverseGUI.pl to detect when that's happening and
+rem automatically close
+call parl -B -OBiodiverseGUI.exe BiodiverseGUI.par

38a4f4e298736ddf19ac169ac0f664b7ce97dffc
shawnlaffan@gmail.com <shawnlaffan@gmail.com@066c9922-a8b7-11de-ac14-117a7284e870>
2012-11-23 00:41:02 +0000

    Script to collapse all the trees in a nexus file, such that all terminal nodes below a cutoff value are raised to be children of their ancestral nodes at that cutoff.

---
+++ trunk/example_scripts/collapse_trees_in_nexus.pl
@@ -0,0 +1,60 @@
+#!/usr/bin/perl -w
+use strict;
+use warnings;
+use 5.010;
+
+use Carp;
+use English qw { -no_match_vars };
+
+local $| = 1;
+
+use Biodiverse::Tree;
+use Biodiverse::ReadNexus;
+
+my $read_nexus = Biodiverse::ReadNexus->new();
+
+my $in_file  = shift @ARGV;
+my $out_file = shift @ARGV;
+my $threshold = shift @ARGV // (2 / 3);
+
+croak "no basedata file specified" if !defined $in_file;
+
+open my $fh, '<', $in_file or croak "Cannot open $in_file";
+
+my @lines;
+my @trees;
+LINE:
+while (defined (my $line = <$fh>)) {
+    if (not $line =~ /^tree/i) {  #  grab this line and store it
+        push @lines, $line;
+        next LINE;
+    }
+    $line =~ /^tree\s+(\w+)\s*=\s*(.+)/;
+    my ($tree_name, $newick) = ($1, $2);
+    
+    say $tree_name;
+
+    my $tree = Biodiverse::Tree->new (NAME => $tree_name);
+    my $count = 0;
+    my $node_count = \$count;
+
+    $read_nexus->parse_newick (
+        string          => $newick,
+        tree            => $tree,
+        node_count      => $node_count,
+    );
+    
+    $tree->collapse_tree_below (target_value => $threshold);
+    
+    
+    my $newick_trimmed = $tree->to_newick;
+    push @lines, "tree $tree_name = $newick_trimmed;\n";
+}
+
+
+my $fname = $out_file;
+open my $ofh, '>', $fname or croak "cannot open $fname";
+
+foreach my $line (@lines) {
+    print { $ofh } $line;
+}

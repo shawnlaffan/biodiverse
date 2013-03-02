7847589e8cf660d00a896906281d2066b2c694eb
shawnlaffan@gmail.com <shawnlaffan@gmail.com@066c9922-a8b7-11de-ac14-117a7284e870>
2013-03-02 05:43:28 +0000

    If ever we release via cpan then we'll use the App namespace.

---
+++ trunk/lib/App/Biodiverse.pm
@@ -0,0 +1,49 @@
+package App::Biodiverse;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.18_004';
+
+1;
+
+
+=head1 NAME
+
+Biodiverse::BaseData
+
+=head1 SYNOPSIS
+
+  use App::Biodiverse;
+  
+=head1 DESCRIPTION
+
+The Biodiverse application.  See http://purl.org/biodiverse for more details.  
+
+All relevant modules are under the Biodiverse namespace.  
+
+=head1 REPORTING ERRORS
+
+Use the issue tracker at http://www.purl.org/biodiverse
+
+=head1 COPYRIGHT
+
+Copyright (c) 2001-present Shawn Laffan. All rights reserved.  
+
+=head1 LICENSE
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+For a full copy of the license see <http://www.gnu.org/licenses/>.
+
+=cut
+
+

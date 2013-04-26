bd37b40847d0126cf5b682a5c2ae0f87622fabd5
Dan Rosauer <DanRosauer@users.noreply.github.com>
2013-04-26 06:11:49 +0000

    A few errors fixed, and some of the extra screen output for debugging removed

--- etc/site_pair_sampler/bin/GDM_Input_sample_params_new_struct.pl
+++ etc/site_pair_sampler/bin/GDM_Input_sample_params_new_struct.pl
@@ -27,11 +27,9 @@ use Biodiverse::Config;
 #  don't buffer text output - output to screen as we go
 local $OUTPUT_AUTOFLUSH = 1;
 
-
 my $args_file = $ARGV[0];
 
 my %dist_args = parse_args_file ($args_file);
-
 
 print "\n\nStarting site pair process\n";
 

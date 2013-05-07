53657bc99baac54e71ae5ba89c1fe9d87841a638
shawnlaffan@gmail.com <shawnlaffan@gmail.com@066c9922-a8b7-11de-ac14-117a7284e870>
2013-05-07 04:34:05 +0000

    Implement the remapping component. Add short forms for most arguments.

--- trunk/example_scripts/duplicate_and_remap_labels.pl
+++ trunk/example_scripts/duplicate_and_remap_labels.pl
@@ -3,7 +3,8 @@ use strict;
 use warnings;
 use 5.010;
 
-#use Getopt::Long;
+use rlib;
+
 use Getopt::Long::Descriptive;
  
 use Carp;
@@ -12,15 +13,18 @@ use English qw { -no_match_vars };
 use Biodiverse::BaseData;
 use Biodiverse::ElementProperties;
 
+exit if $ENV{BDV_PP_BUILDING};
+
 my ($opt, $usage) = describe_options(
   '%c <arguments>',
-  [ 'basedata|input_bd=s',  "the input basedata file", { required => 1 } ],
-  [ 'out_file|output_bd=s', "the output basedata file", { required => 1 }],
-  [ 'remap_file=s',         "the text file containing label remap details", { required => 1 } ],
-  [ 'input_cols=s',         "the columns in the remap file that match the original labels", { default => '1' } ],
-  [ 'remapped_cols=s',      "the columns in the remap file to generate the remapped labels", { default => '1,2' } ],
-  [ 'input_sep_char=s',     "column separator character in the remap file", { default => q{,} } ],
-  [ 'input_quote_char=s',   "quotes character in the remap file", { default => q{"} } ],
+  [ 'basedata|input_bd=s',   'the input basedata file', { required => 1 } ],
+  [ 'out_file|output_bd=s',  'the output basedata file', { required => 1 }],
+  [ 'remap_file|rf=s',       'the text file containing label remap details', { required => 1 } ],
+  [ 'input_cols|ic=s',       'the columns in the remap file that match the original labels', { default => '1' } ],
+  [ 'remapped_cols|rc=s',    'the columns in the remap file to generate the remapped labels', { default => '1,2' } ],
+  [ 'input_sep_char|is=s',   'column separator character in the remap file', { default => q{,} } ],
+  [ 'input_quote_char|iq=s', 'quotes character in the remap file', { default => q{"} } ],
+  [ 'verbose|v!',            'warns if labels are not remapped', { default => 1 } ],
   [],
   [ 'help',       "print usage message and exit" ],
 );
@@ -37,17 +41,15 @@ my $input_cols_str    = $opt->input_cols;
 my $remapped_cols_str = $opt->remapped_cols;
 my $input_sep_char    = $opt->input_sep_char;
 my $input_quote_char  = $opt->input_quote_char;
+my $verbose           = $opt->verbose;
 
 
+my $bd = eval {Biodiverse::BaseData->new(file => $bd_file)};
+croak $@ if $@;
 
-croak "no basedata file specified" if !defined $bd_file;
-croak "no output file specified"   if !defined $out_file;
-croak "no remap file specified"    if !defined $remap_file;
-
-my $bd = Biodiverse::BaseData->new(file => $bd_file);
-
-my @input_cols    = split ',', $input_cols_str;
-my @remapped_cols = split ',', $remapped_cols_str;
+#  flip the order since we want one to many, and props do one to one
+my @remapped_cols = split ',', $input_cols_str;
+my @input_cols    = split ',', $remapped_cols_str;
 
 my $remap = Biodiverse::ElementProperties->new();
 $remap->import_data (
@@ -58,3 +60,42 @@ $remap->import_data (
     input_quote_char      => $input_quote_char,
 );
 
+
+#  Get a hash of arrays, where each original label has an array of labels it is remapped to
+my %label_remap_hash;
+foreach my $element ($remap->get_element_list()) {
+    my $bd_label = $remap->get_element_remapped(element => $element);
+    if (!exists $label_remap_hash{$bd_label}) {
+        $label_remap_hash{$bd_label} = [];
+    }
+    push @{$label_remap_hash{$bd_label}}, $element;
+}
+
+#  create the new basedata to be populated
+my $new_bd = Biodiverse::BaseData->new(
+    CELL_SIZES => $bd->get_param('CELL_SIZES'),
+    NAME       => $bd->get_param('NAME'),
+);
+
+#  Now iterate over the original basedata, copying across remapped labels
+#  Warn when labels aren't remapped.
+BD_LABEL:
+foreach my $bd_label ($bd->get_labels) {
+    my $remapped = $label_remap_hash{$bd_label};
+    if (!$remapped && $verbose) {
+        warn "Label $bd_label has not been remapped\n";
+        $remapped = [$bd_label];
+        #next BD_LABEL;
+    }
+    
+    my %bd_groups = $bd->get_groups_with_label_as_hash(label => $bd_label);
+    foreach my $new_label (@$remapped) {
+        while (my ($gp, $count) = each %bd_groups) {
+            $new_bd->add_element (label => $new_label, group => $gp, count => $count);
+        }
+    }
+}
+
+$new_bd->save (filename => $out_file);
+
+say 'Completed';

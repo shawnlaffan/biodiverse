62966c01939c8a76c5da634d0ab85961c98de01e
shawnlaffan@gmail.com <shawnlaffan@gmail.com@066c9922-a8b7-11de-ac14-117a7284e870>
2013-09-20 04:46:49 +0000

    Update issue #402 Create branch to work on this issue.

---
+++ branches/issue402_label_props_range_weighted/lib/Biodiverse/RegionGrower.pm
@@ -0,0 +1,52 @@
+package Biodiverse::RegionGrower;
+use strict;
+use warnings;
+
+our $VERSION = '0.18_007';
+
+use parent qw /
+    Biodiverse::Cluster
+/;
+
+
+our %PARAMS = (  #  most of these are not used
+    DEFAULT_CLUSTER_INDEX => 'RICHNESS_ALL',
+    DEFAULT_LINKAGE       => 'link_recalculate',
+    TYPE                  => 'RegionGrower',
+    OUTSUFFIX             => 'bts',
+    #OUTSUFFIX_XML         => 'btx',
+    OUTSUFFIX_YAML        => 'bty',
+    OUTPUT_QUOTE_CHAR     => q{"},
+    OUTPUT_SEP_CHAR       => q{,},
+    COMPLETED             => 0,
+);
+
+
+#  use the new sub from Cluster
+
+#  need to modify to use something else
+sub get_default_cluster_index {
+    return $PARAMS{DEFAULT_CLUSTER_INDEX};
+}
+
+sub get_type {
+    return $PARAMS{TYPE};
+}
+
+sub get_valid_indices_sub {
+    return 'get_valid_region_grower_indices';
+}
+
+#  Get a list of the all the publicly available linkages.
+#  Overrides those in Biodiverse::Cluster
+#  Will need to restructure if we allow other linkages
+sub get_linkage_functions {  
+    my $self = shift;
+
+    my @linkages = qw /link_recalculate/;
+
+    return wantarray ? @linkages : \@linkages;
+}
+
+
+1;

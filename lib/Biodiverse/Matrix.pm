13297747b15b6dc9da944f5026cbe2292d5149c5
shawnlaffan@gmail.com <shawnlaffan@users.noreply.github.com>
2013-09-21 08:49:28 +0000

    Update issue #403 Merge changes from the branch.

--- lib/Biodiverse/Matrix.pm
+++ lib/Biodiverse/Matrix.pm
@@ -13,7 +13,7 @@ use English ( -no_match_vars );
 
 use Carp;
 use Data::Dumper;
-use Scalar::Util qw /looks_like_number/;
+use Scalar::Util qw /looks_like_number blessed/;
 use List::Util qw /min max sum/;
 use File::BOM qw /:subs/;
 
@@ -82,7 +82,41 @@ sub rename {
 
 
 #  avoid needless cloning of the basedata, but don't create the parameter if it is not already there
+#sub clone {
+#    my $self = shift;
+#    my %args = @_;
+#    
+#    my $bd;
+#    my $exists = $self->exists_param('BASEDATA_REF');
+#    if ($exists) {
+#        $bd = $self->get_param('BASEDATA_REF');
+#        $self->set_param(BASEDATA_REF => undef);
+#    }
+#
+#    my $clone_ref = eval {
+#        $self->SUPER::clone(%args);
+#    };
+#    if ($EVAL_ERROR) {
+#        if ($exists) {
+#            $self->set_param(BASEDATA_REF => $bd);  #  put it back if needed
+#        }
+#        croak $EVAL_ERROR;
+#    }
+#
+#    if ($exists) {
+#        $self->set_param(BASEDATA_REF => $bd);
+#        $clone_ref->set_param(BASEDATA_REF => $bd);
+#    }
+#
+#    return $clone_ref;
+#}
+
 sub clone {
+    my $self = shift;
+    return $self->_duplicate (@_);
+}
+
+sub _duplicate {
     my $self = shift;
     my %args = @_;
     
@@ -93,9 +127,54 @@ sub clone {
         $self->set_param(BASEDATA_REF => undef);
     }
 
-    my $clone_ref = eval {
-        $self->SUPER::clone(%args);
+    my $params = eval {
+        $self->SUPER::clone(data => $self->{PARAMS});
     };
+
+    my $clone_ref = blessed ($self)->new(%$params);
+
+    my $elements = $self->get_elements_ref;
+
+    my $c_elements_ref = $clone_ref->get_elements_ref;
+    %$c_elements_ref = %$elements;
+
+    #  should add methods to access these
+    my $byelement = $self->{BYELEMENT};
+    my $byvalue   = $self->{BYVALUE};
+
+    my (%c_byelement, %c_byvalue);
+    keys %c_byelement = keys %$byelement;  #  pre-allocate the buckets - the pay-off is for many-key hashes
+    keys %c_byvalue   = keys %$byvalue;
+
+    foreach my $key (keys %$byelement) {
+        my $hashref = $byelement->{$key};
+        my %c_hash;
+        keys %c_hash = keys %$hashref;  #  pre-allocate the buckets
+        %c_hash = %$hashref;
+        $c_byelement{$key} = \%c_hash;
+    }
+
+    $clone_ref->{BYELEMENT} = \%c_byelement;
+
+    foreach my $val_key (keys %$byvalue) {
+        my $val_hashref = $byvalue->{$val_key};
+        my %c_val_hash;
+        keys %c_val_hash = keys %$val_hashref;  #  pre-allocate the buckets
+
+        foreach my $e_key (keys %$val_hashref) {
+            my $hashref = $val_hashref->{$e_key};
+            my %c_hash;
+            keys %c_hash = keys %$hashref;  #  pre-allocate the buckets
+            %c_hash = %$hashref;
+            $c_val_hash{$e_key} = \%c_hash;
+        }
+
+        $c_byvalue{$val_key} = \%c_val_hash;
+    }
+
+    $clone_ref->{BYVALUE} = \%c_byvalue;
+
+
     if ($EVAL_ERROR) {
         if ($exists) {
             $self->set_param(BASEDATA_REF => $bd);  #  put it back if needed
@@ -838,6 +917,12 @@ sub get_elements {
     return if (scalar keys %{$self->{ELEMENTS}}) == 0;
 
     return wantarray ? %{$self->{ELEMENTS}} : $self->{ELEMENTS};
+}
+
+sub get_elements_ref {
+    my $self = shift;
+
+    return $self->{ELEMENTS} // do {$self->{ELEMENTS} = {}};
 }
 
 sub get_elements_as_array {

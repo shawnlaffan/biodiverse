ccdd772369464255d848f16b2d981f723ae487ec
shawnlaffan@gmail.com <shawnlaffan@gmail.com@066c9922-a8b7-11de-ac14-117a7284e870>
2013-10-29 03:44:37 +0000

    Generate indices table for version 0.19. Also update the script to provide more useful examples of the label and group property calculations.

--- wiki/build_indices_table.pl
+++ wiki/build_indices_table.pl
@@ -3,6 +3,8 @@ use strict;
 use warnings;
 use Carp;
 
+use English qw { -no_match_vars };
+
 BEGIN {
     $ENV{BIODIVERSE_EXTENSIONS_IGNORE} = 1;
     $ENV{BIODIVERSE_EXTENSIONS} = q{};
@@ -11,7 +13,9 @@ BEGIN {
 use Biodiverse::Config;
 use Biodiverse::BaseData;
 use Biodiverse::Indices;
+use Biodiverse::ElementProperties;
 use Data::Dumper;
+use File::Temp;
 
 #use HTML::HashTable;
 use HTML::QuickTable;
@@ -25,6 +29,26 @@ $bd->add_element (
 );
 my $lb_ref = $bd->get_labels_ref;
 $lb_ref->set_param (CELL_SIZES => [-1,-1]);
+
+my $label_props = get_label_properties();
+eval {
+    $bd->assign_element_properties (
+        type              => 'labels', # plural
+        properties_object => $label_props,
+    )
+};
+my $e = $EVAL_ERROR;
+warn $e if $e;
+
+my $group_props = get_group_properties();
+eval {
+    $bd->assign_element_properties (
+        type              => 'groups', # plural
+        properties_object => $group_props,
+    )
+};
+my $e = $EVAL_ERROR;
+warn $e if $e;
 
 my $indices = Biodiverse::Indices->new(BASEDATA_REF => $bd);
 
@@ -62,7 +86,7 @@ information visible in the GUI, plus some additional details.
 
 Most of the headings are self-explanatory.  For the others:
   * The *Subroutine* is the name of the subroutine used to call the function if you are using Biodiverse through a script.  
-  * The *Index* is the name of the index in the SPATIAL_RESULTS list, or if it is its own list then this will be its name.  These lists can contain a variety of values, but are usually lists of labels with some value, for example the weights used in an endemism calculation.  The names of such lists typically end in "LIST", "ARRAY", "HASH" or "LABELS". 
+  * The *Index* is the name of the index in the SPATIAL_RESULTS list, or if it is its own list then this will be its name.  These lists can contain a variety of values, but are usually lists of labels with some value, for example the weights used in an endemism calculation.  The names of such lists typically end in "LIST", "ARRAY", "HASH", "LABELS" or "STATS". 
   * *Valid cluster metric* is whether or not the index can be used as a clustering metric.  A blank value means it cannot.
   * The *Minimum number of neighbour sets* dictates whether or not a calculation or index will be run.  If you specify only one neighbour set then all those calculations that require two sets will be dropped from the analysis.  (This is always the case for calculations applied to cluster nodes as there is only one neighbour set, defined by the set of groups linked to the terminal nodes below a cluster node).  Note that many of the calculations lump neighbour sets 1 and 2 together.  See the SpatialConditions page for more details on neighbour sets.
 
@@ -103,3 +127,58 @@ print $fh $wiki_text;
 close $fh;
 
 print "done\n";
+
+sub get_label_properties {
+    my $data = <<'END_LABEL_PROPS'
+ax1,ax2,example_prop1,example_prop2
+a,b,1,1,1
+END_LABEL_PROPS
+  ;
+  
+    element_properties_from_string($data);
+}
+
+sub get_group_properties {
+    my $data = <<'END_LABEL_PROPS'
+ax1,ax2,example_gprop1,example_gprop2
+1,1,1,1,1
+END_LABEL_PROPS
+  ;
+
+    element_properties_from_string($data);
+}
+
+sub element_properties_from_string {
+    my ($data) = @_;
+    my $file = write_data_to_temp_file($data);
+    my $props = Biodiverse::ElementProperties->new;
+
+    # Get property column names and positions. First is 2.
+    # Results in something like:
+    # (LBPROP1 => 3, LBPROP2 => 4, ...)
+    my $i = 2;
+    $data =~ m/^(.*)$/m;
+    my @prop_names = split ',', $1;
+    my %prop_cols = map { $_ => $i++; } @prop_names[2..$#prop_names];
+
+    my $success = eval { $props->import_data (
+        input_element_cols    => [0,1],
+        file                  => $file,
+        %prop_cols, # Tell import_data which columns contain which properties
+    ) };
+    my $e = $EVAL_ERROR;
+    warn $e if $e;
+
+    return $props;
+}
+
+sub write_data_to_temp_file {
+    my $data = shift;
+
+    my $tmp_obj = File::Temp->new;
+    my $fname = $tmp_obj->filename;
+    print $tmp_obj $data;
+    $tmp_obj->close;
+
+    return $tmp_obj;
+}

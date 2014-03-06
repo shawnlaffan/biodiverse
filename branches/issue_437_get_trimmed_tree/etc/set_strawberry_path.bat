f155c72e274c49e136d8dd61b8f0f767aef8db12
shawnlaffan@gmail.com <shawnlaffan@gmail.com@066c9922-a8b7-11de-ac14-117a7284e870>
2014-03-06 03:27:44 +0000

    Fixes issue 437 All is done now so delete the branch.

--- branches/issue_437_get_trimmed_tree/etc/set_strawberry_path.bat
+++
@@ -1,7 +0,0 @@
-:: Set the environment for this strawberry perl
-:: To use it, copy this file into the top level of a strawberry perl installation.
-
-
-set ST_PATH=%~dp0
-set PATH=%ST_PATH%c\bin;%ST_PATH%perl\site\bin;%ST_PATH%perl\bin;%PATH%
-

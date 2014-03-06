a5e2f5ed56cf865a8873ade2901f5bf1910a92fe
shawnlaffan@gmail.com <shawnlaffan@gmail.com@066c9922-a8b7-11de-ac14-117a7284e870>
2014-03-06 03:06:43 +0000

    Update issue 440 Add branch to work on this issue.

---
+++ branches/isse_440_use_pairkeys/BiodiverseGUI.bat
@@ -0,0 +1,24 @@
+:: Change to the directory that this batch file is in
+:: NB: it must be invoked with a full path, as should any arguments!
+::  This is because it is a bodge and not a full solution.
+
+:: set oldcd=%CD%
+:: for /f %%i in ("%0") do set curpath=%%~dpi
+:: cd /d %curpath% 
+
+set batch_path=%~dp0
+rem set GTK_BASEPATH=%batch_path%gtk\bin
+#set BIODIVERSE_BIN_PATH=%batch_path%bin
+rem set PATH=%GTK_BASEPATH%;%BIODIVERSE_BIN_PATH%;%PATH%
+#set PATH=%BIODIVERSE_BIN_PATH%;%PATH%
+
+:: set PATH
+
+:: set argument=exec (DIR /B /S %1)
+
+perl %batch_path%\bin\BiodiverseGUI.pl %1
+
+:: cd /d %oldcd%
+
+pause
+

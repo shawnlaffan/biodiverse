afc5c243df722742eedef29ed0c8a9743e8941b6
shawnlaffan <shawnlaffan@066c9922-a8b7-11de-ac14-117a7284e870>
2014-10-20 03:11:47 +0000

    Add branch to work on this issue

---
+++ branches/issue_278_null_value_display_colour/lib/Biodiverse/Metadata.pm
@@ -0,0 +1,14 @@
+package Biodiverse::Metadata;
+use strict;
+use warnings;
+
+our $VERSION = '0.99_005';
+
+sub new {
+    my $class = shift;
+    my $self = bless {}, $class;
+    return $self;
+}
+
+
+1;

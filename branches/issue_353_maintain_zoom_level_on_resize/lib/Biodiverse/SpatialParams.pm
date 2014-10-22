53f6958c4d6ae06b26ffdbafc44cd392367ecf63
shawnlaffan <shawnlaffan@066c9922-a8b7-11de-ac14-117a7284e870>
2014-10-22 04:12:21 +0000

    Add branch to work on maintaining the zoom level

---
+++ branches/issue_353_maintain_zoom_level_on_resize/lib/Biodiverse/SpatialParams.pm
@@ -0,0 +1,15 @@
+package Biodiverse::SpatialParams;
+
+use 5.010;
+use strict;
+use warnings;
+
+#  This is a stub for backwards compatibility.
+#  All the content is now in Biodiverse::SpatialConditions
+
+our $VERSION = '0.99_005';
+
+use parent qw /Biodiverse::SpatialConditions/;
+
+
+1;

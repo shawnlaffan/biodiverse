2c16d4de9c8d8a4f65daad2efb47c8915f3794b4
shawnlaffan <shawnlaffan@users.noreply.github.com>
2014-11-16 04:16:57 +0000

    Update issue 471 Dequote in all places where add_element method is called with input or transformed data. Clean up some of Hierarchical_Labels.pm while we are about it.

--- lib/Biodiverse/BaseData.pm
+++ lib/Biodiverse/BaseData.pm
@@ -1296,6 +1296,10 @@ sub import_data_raster {
                     $this_label = "band$b";
                 }
             }
+            $this_label = $self->dequote_element (
+                element    => $this_label,
+                quote_char => $quotes,
+            );
 
             # get category names for this band, which will attempt
             # to be used as labels based on cell values (if ! labels_as_bands)

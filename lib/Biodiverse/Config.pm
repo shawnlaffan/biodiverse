4fef5be84a97295381613c9948be999804b57da0
shawnlaffan <shawnlaffan@users.noreply.github.com>
2014-11-25 01:46:09 +0000

    Add an environment variable flag to stop biodiverse looking for the gdal and gtk folders on windows.  (Prev commit did the wrong file)

--- lib/Biodiverse/Config.pm
+++ lib/Biodiverse/Config.pm
@@ -48,7 +48,7 @@ BEGIN {
     #  Add the gtk and gdal libs if using windows - brittle?
     #  Search up the tree until we find a dir of the requisite name
     #  and which contains a bin folder
-    if ($OSNAME eq 'MSWin32') {
+    if ($OSNAME eq 'MSWin32' && !$ENV{BD_NO_LIB_SEARCH}) {
         #say "PAR_PROGNAME: $ENV{PAR_PROGNAME}";
         my $prog_name  = $ENV{PAR_PROGNAME} || $Bin;
         
@@ -93,7 +93,6 @@ BEGIN {
 
 
 #  add biodiverse lib paths so we get all the extensions
-#  should be a sub not a begin block
 sub add_lib_paths {
     my $var = shift // 'BIODIVERSE_LIB';
 

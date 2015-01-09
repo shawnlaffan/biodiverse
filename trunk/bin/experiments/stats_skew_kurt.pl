ea5822bb24fb698b8d5ee1847b6c39c387a4adec
shawnlaffan <shawnlaffan@066c9922-a8b7-11de-ac14-117a7284e870>
2015-01-09 04:45:21 +0000

    Make the skewness and kurtosis faster still, following more benchmarking.

--- trunk/bin/experiments/stats_skew_kurt.pl
+++ trunk/bin/experiments/stats_skew_kurt.pl
@@ -11,18 +11,20 @@ use List::MoreUtils;
 
 $| = 1;
 
-#my $d_hash1 = get_data(100, 2);
+#my $data = get_data(100, 2);
 #my $d_hash2 = get_data(50,  2);
-#say join " ", old_school (label_hash1 => $d_hash1, label_hash2 => $d_hash2);
-#say join " ", new_school (label_hash1 => $d_hash1, label_hash2 => $d_hash2);
+#say join " ", old_school (label_hash1 => $data, label_hash2 => $d_hash2);
+#say join " ", new_school (label_hash1 => $data, label_hash2 => $d_hash2);
 
-
+use constant POWER => 3;
+my $mean = 1 / 3;
+my $sd = 1 / 7;
 
 
 my @data_size_and_reps = (
-    [100   => -1],   #  short sequence, many benchmark reps
-    [1000  => -1],   #  mid range sequence, many benchmark reps
-    [100000 => -1],
+    [100   => -1],   #  short sequence
+    [1000  => -1],   #  mid range sequence
+    #[100000 => -1],
 );
 
 
@@ -34,11 +36,13 @@ foreach my $pair (@data_size_and_reps) {
         say '=' x 30;
         say "Data size: $n, reps: $reps, iter: $iter";
 
-        my $d_hash1 = get_data($n, $iter);
+        my $data = get_data($n, $iter);
     
         cmpthese($reps, {
-            'Old School' => sub {old_school (data => $d_hash1)},
-            'New School' => sub {new_school (data => $d_hash1)},
+            'Pre School' => sub {pre_school (data => $data)},
+            #'Old School' => sub {old_school (data => $data)},
+            #'New School' => sub {new_school (data => $data)},
+            'Day Care'   => sub {day_care   (data => $data)},
         });
     }
 }
@@ -46,15 +50,39 @@ foreach my $pair (@data_size_and_reps) {
 say '=' x 30;
 say "Completed";
 
-sub old_school {
+sub day_care {
+    my %args = @_;
+    
+    my $data = $args{data};
+
+    my $sum_pow4;
+    foreach my $rec ( @$data ) {
+        my $val = (($rec - $mean) / $sd) ** POWER;
+        $sum_pow4 += $val;
+    }
+
+}
+
+
+sub pre_school {
     my %args = @_;
     
     my $data = $args{data};
     
-    my $mean = 0.1;
-    my $sd   = 0.1;
+    my $sum_pow4;
+    foreach my $rec ( @$data ) {
+        $sum_pow4 +=  (($rec - $mean) / $sd) ** POWER;
+    }
 
-    my @tmp = List::MoreUtils::apply { $_ = (($_ - $mean) / $sd) ** 4 } @$data;
+}
+
+
+sub old_school {
+    my %args = @_;
+    
+    my $data = $args{data};
+
+    my @tmp = List::MoreUtils::apply { $_ = (($_ - $mean) / $sd) ** POWER } @$data;
     my $sum_pow4 = List::Util::sum @tmp;
 
 }
@@ -62,12 +90,9 @@ sub old_school {
 sub new_school {
     my %args = @_;
 
-    my $mean = 0.1;
-    my $sd   = 0.1;
-
     my $data = $args{data};
 
-    my $sum_pow4 = List::Util::sum map { (($_ - $mean) / $sd) ** 4 } @$data;
+    my $sum_pow4 = List::Util::sum map { (($_ - $mean) / $sd) ** POWER } @$data;
 
 }
 

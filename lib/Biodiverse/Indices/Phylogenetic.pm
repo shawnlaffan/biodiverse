3cae67e1781d662a53b28fb9ec49f4b0e35d53b1
shawnlaffan <shawnlaffan@users.noreply.github.com>
2015-02-05 01:35:38 +0000

    Use a binary search to reduce the hash slice assignments.  Avoids linear grepping.

--- lib/Biodiverse/Indices/Phylogenetic.pm
+++ lib/Biodiverse/Indices/Phylogenetic.pm
@@ -350,13 +350,33 @@ sub get_path_lengths_to_root_node {
 
         if (!$sub_path) {
             my $current_node = $all_nodes->{$label};
-            $sub_path = $current_node->get_path_lengths_to_root_node (cache => $cache);
+            #$sub_path = $current_node->get_path_lengths_to_root_node (cache => $cache);
+            $sub_path = $current_node->get_path_to_root_node (cache => $cache);
             $path_cache->{$current_node} = $sub_path;
         }
 
-        #  Assign lengths later in one pass
-        #  The grep is to try to reduce assignment overheads in the slice
-        @path{grep {!exists $path{$_}} keys %$sub_path} = undef;  
+        my @node_names = map {$_->get_name} @$sub_path;
+        #  Assign lengths later in one pass.
+        if (!scalar keys %path) {
+            @path{@node_names} = ();
+        }
+        else {
+            #  Use a binary search to avoid adding seen nodes,
+            #  thus avoiding repeated assignments in the slice.
+            my ($b_lower, $b_upper) = (0, $#node_names);
+            my $b_mid = int ($b_upper / 2);
+            while ($b_lower != $b_mid && $b_upper != $b_mid) {
+                if (exists $path{$node_names[$b_mid]}) {
+                    $b_upper = $b_mid;
+                }
+                else {
+                    $b_lower = $b_mid;
+                }
+                $b_mid = int ($b_lower + ($b_upper - $b_lower) / 2);
+            }
+            
+            @path{@node_names[0 .. $b_mid]} = ();
+        }
     }
 
     #  Assign the lengths once each.

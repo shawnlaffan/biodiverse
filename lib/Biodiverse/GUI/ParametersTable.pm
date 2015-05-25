f15441dbd9b6f113530f61cbd7a41773842b1564
Shawn Laffan <shawnlaffan@gmail.com>
2015-05-25 21:59:31 +1000

    Comment out some debug code
    Signed-off-by: Shawn Laffan <shawnlaffan@gmail.com>

--- lib/Biodiverse/GUI/ParametersTable.pm
+++ lib/Biodiverse/GUI/ParametersTable.pm
@@ -87,10 +87,10 @@ sub fill {
         # The extractor will be called after the dialogue is OK'd to get the parameter value
         # It returns (param_name, value)
 #  debug
-use Scalar::Util qw /blessed/;
-if (!blessed $param) {
-    warn "Param not blessed";
-}
+#use Scalar::Util qw /blessed/;
+#if (!blessed $param) {
+#    warn "Param not blessed";
+#}
         my ($widget, $extractor) = generate_widget($param, $dlgxml);
 
         if ($extractor) {

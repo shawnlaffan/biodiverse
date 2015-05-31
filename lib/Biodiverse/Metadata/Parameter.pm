c94de02db419619003378f46f8c805ddf4d1cb45
Shawn Laffan <shawnlaffan@gmail.com>
2015-05-31 20:13:46 +1000

    Delete some commented, and redundant, code.
    Signed-off-by: Shawn Laffan <shawnlaffan@gmail.com>

--- lib/Biodiverse/Metadata/Parameter.pm
+++ lib/Biodiverse/Metadata/Parameter.pm
@@ -42,47 +42,6 @@ sub _get_method_default_hash {
     return wantarray ? %methods_and_defaults : {%methods_and_defaults};
 }
 
-#sub _make_access_methods {
-#    my ($pkg, $methods) = @_;
-#
-#    no strict 'refs';
-#    foreach my $key (keys %$methods) {
-#        *{$pkg . '::' . 'get_' . $key} =
-#            do {
-#                sub {
-#                    my $self = shift;
-#                    return $self->{$key} // $self->get_default_value ($key);
-#                };
-#            };
-#        *{$pkg . '::' . 'set_' . $key} =
-#            do {
-#                sub {
-#                    my ($self, $val) = @_;
-#                    $self->{$key} = $val;
-#                };
-#            };
-#    }
-#
-#    return;
-#}
-#
-#sub get_default_value {
-#    my ($self, $key) = @_;
-#
-#    #  set defaults - make sure they are new each time
-#    my $default = $methods_and_defaults{$key};
-#
-#    return $default if !defined $default or !reftype $default;
-#
-#    if (reftype ($default) eq 'ARRAY') {
-#        $default = [];
-#    }
-#    elsif (reftype ($default) eq 'HASH') {
-#        $default = {};
-#    }
-#    return $default;
-#}
-
 __PACKAGE__->_make_access_methods (\%methods_and_defaults);
 
 

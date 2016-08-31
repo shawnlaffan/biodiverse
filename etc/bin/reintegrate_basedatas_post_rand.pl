2d0e399097175a1100398d89199547ba9a685479
Shawn Laffan <shawnlaffan@gmail.com>
2016-09-01 09:35:49 +1000

    reintegrate script uses the no_verify option by default
    Update #558

--- etc/bin/reintegrate_basedatas_post_rand.pl
+++ etc/bin/reintegrate_basedatas_post_rand.pl
@@ -10,6 +10,7 @@ my ($opt, $usage) = describe_options(
   '%c <arguments>',
   [ 'glob=s',     'glob to find files', { required => 1 } ],
   [ 'output_prefix|opfx=s', 'The output prefix for exported files', {required => 1}],
+  [ 'no_verify|n!', 'do not verify that the basedatas match', { default => 1} ],
   [],
   [ 'help',       "print usage message and exit" ],
 );
@@ -17,6 +18,7 @@ my ($opt, $usage) = describe_options(
 
 my $glob = $opt->glob;
 my $opfx = $opt->output_prefix;
+my $no_verify = $opt->no_verify;
 
 my @files = glob $glob;
 
@@ -32,6 +34,7 @@ foreach my $from_file (@files) {
     my $from_bd = Biodiverse::BaseData->new(file => $from_file);
     $bd->reintegrate_after_parallel_randomisations (
         from => $from_bd,
+        no_check_groups_and_labels => $no_verify,
     );
     
 }

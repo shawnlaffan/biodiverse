6928504032e0ed5a351975acff05cec4ffd06f69
Luke Fitzpatrick <lukedfitzpatrick@gmail.com>
2016-11-22 16:48:35 +1100

    extracted the guess_remap function into its own module

--- lib/Biodiverse/GUI/PhylogenyImport.pm
+++ lib/Biodiverse/GUI/PhylogenyImport.pm
@@ -11,7 +11,7 @@ use Gtk2;
 use Biodiverse::ReadNexus;
 use Biodiverse::GUI::BasedataImport;
 use Biodiverse::GUI::YesNoCancel;
-use Text::Levenshtein qw(distance);
+use Biodiverse::RemapGuesser;
 
 our $VERSION = '1.99_006';
 
@@ -214,8 +214,14 @@ sub run {
 	foreach my $tree (@$phylogeny_array) {
 	    my %named_nodes = $tree->get_named_nodes();
 	    my @tree_labels = (keys %named_nodes);
-	    my ($furthest, $mean, %remap) = guess_remap(\@basedata_labels, \@tree_labels);
 
+	    # furthest and mean refer to the largest string
+	    # distance which resulted in a match and the mean match
+	    # string distance. Could be useful to give an idea of how
+	    # good the remap is, not used for now.
+	    my ($furthest, $mean, %remap) = Biodiverse::RemapGuesser::guess_remap(\@basedata_labels, \@tree_labels);
+
+	    
 	    # debug output and user message
 	    my $remap_text = "\n\n";
 	    say "[Phylogeny Import] Generated the following guessed remap:";
@@ -569,42 +575,6 @@ sub add_column {
     $col_widgets->[$col_id] = [$radio1, $radio2, $radio3];
 }
 
-
-# takes in two references to arrays of labels
-# returns a hash mapping labels in the second list to labels in the first list
-# can be used to generate a remap table (Element Property Table)
-sub guess_remap {
-    my ($first_ref, $second_ref) = @_;
-    my @first_labels = @{$first_ref};
-    my @second_labels = @{$second_ref};
-    my %remap;
-
-    # also keep track of the furthest distance we have to accept,
-    # and the mean distance, so we get an idea of how good this remap is.
-    my $furthestDistance = 0;
-    my $distanceSum = 0;
-    
-    foreach my $label (@second_labels) {
-	my $min_distance = distance($label, $first_labels[0]);
-	my $closest_label = $first_labels[0];
-
-	# find the closest match (will default to the last in case of a tie)
-	foreach my $comparison_label (@first_labels) {
-	    my $this_distance = distance($label, $comparison_label);
-	    if($this_distance <= $min_distance) {
-		$min_distance = $this_distance;
-		$closest_label = $comparison_label;
-	    }
-	}
-
-	$furthestDistance = $min_distance if($min_distance > $furthestDistance);
-	$distanceSum += $min_distance;
-	$remap{$label} = $closest_label;
-    }
-
-    my $meanDistance = $distanceSum/($#second_labels+1);
-    return ($furthestDistance, $meanDistance, %remap);
-}
 
 
 
---
+++ lib/Biodiverse/RemapGuesser.pm
@@ -0,0 +1,56 @@
+package Biodiverse::RemapGuesser;
+
+# guesses appropriate remappings between labels.
+# canonical examples:
+#     mapping "genus_species" to "genus species"
+#             "genus:species" to "genus_species"
+#             "Genus_species" to "genus:species" etc.
+
+use 5.010;
+use strict;
+use warnings;
+
+use Text::Levenshtein qw(distance);
+
+our $VERSION = '1.99_006';
+    
+# takes in two references to arrays of labels
+# returns a hash mapping labels in the second list to labels in the first list
+# can be used to generate a remap table (Element Property Table)
+sub guess_remap {
+    my ($first_ref, $second_ref) = @_;
+    my @first_labels = @{$first_ref};
+    my @second_labels = @{$second_ref};
+    my %remap;
+
+    # also keep track of the furthest distance we have to accept,
+    # and the mean distance, so we get an idea of how good this remap is.
+    my $furthestDistance = 0;
+    my $distanceSum = 0;
+    
+    foreach my $label (@second_labels) {
+	my $min_distance = distance($label, $first_labels[0]);
+	my $closest_label = $first_labels[0];
+
+	# find the closest match (will default to the last in case of a tie)
+	foreach my $comparison_label (@first_labels) {
+	    my $this_distance = distance($label, $comparison_label);
+	    if($this_distance <= $min_distance) {
+		$min_distance = $this_distance;
+		$closest_label = $comparison_label;
+	    }
+	}
+
+	$furthestDistance = $min_distance if($min_distance > $furthestDistance);
+	$distanceSum += $min_distance;
+	$remap{$label} = $closest_label;
+    }
+
+    my $meanDistance = $distanceSum/($#second_labels+1);
+    return ($furthestDistance, $meanDistance, %remap);
+}
+
+
+
+
+1;

67f09575f7a3ac71fa8c918a49e5a40f8baa5636
Luke Fitzpatrick <lukedfitzpatrick@gmail.com>
2016-12-19 17:22:34 +1100

    Remove reftype usage in Index.pm
    Updates #635

--- lib/Biodiverse/Index.pm
+++ lib/Biodiverse/Index.pm
@@ -312,11 +312,11 @@ sub get_index_elements {
         my $reftype_el = reftype ($element) // q{};
         my $reftype_of = reftype ($offset)  // q{};
 
-        my @elements = ($reftype_el eq 'ARRAY')  #  is it an array already?
+        my @elements = (is_arrayref($element))  #  is it an array already?
             ? @$element
             : $self->csv2list (string => $element, csv_object => $csv_object);
 
-        my @offsets = ($reftype_of eq 'ARRAY')  #  is it also an array already?
+        my @offsets = (is_arrayref($offset))  #  is it also an array already?
             ? @$offset
             : $self->csv2list (string => $offset, csv_object => $csv_object);
 
@@ -356,7 +356,7 @@ sub get_index_elements {
                     $hashref->{$col} = $x;
                 };
         }
-        if (reftype ($hashref)) {
+        if (is_ref($hashref)) {
             $element = $self->list2csv(list => \@elements, csv_object => $csv_object);
             $prev_hashref->{$elements[-1]} = $element;
         }

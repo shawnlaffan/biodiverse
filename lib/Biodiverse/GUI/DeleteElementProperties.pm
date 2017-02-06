d1d5c3a5fee855e62d1e5367d06bffd4e39278b9
Luke Fitzpatrick <lukedfitzpatrick@gmail.com>
2017-02-07 10:47:29 +1100

    Add function to reformat element properties hash.
    Updates #547

--- lib/Biodiverse/GUI/DeleteElementProperties.pm
+++ lib/Biodiverse/GUI/DeleteElementProperties.pm
@@ -35,11 +35,40 @@ sub run {
     
     # first build up the data structures to populate the gui.
 
+    # start by doing just the labels, add in the groups later.
     my %el_props_hash = $bd->get_all_element_properties();
+    %el_props_hash = %{$el_props_hash{labels}};
 
-    say "el_props_hash:";
-    use Data::Dumper;
-    print Dumper(\%el_props_hash);
+    #say "el_props_hash:";
+    #use Data::Dumper;
+    #print Dumper(\%el_props_hash);
+
+    # break up into a hash mapping from the property name to a hash
+    # mapping from element name to value
+    %el_props_hash = 
+        $self->format_element_properties_hash( props_hash => \%el_props_hash );
     
 
+}
+
+# given a hash mapping from element name to a hash mapping from
+# property name to value. Convert this to a hash mapping from property
+# name to a hash mapping from element name to value.
+sub format_element_properties_hash {
+    my ($self, %args) = @_;
+    my %old_hash = %{$args{props_hash}};
+    my %new_hash;
+
+    foreach my $element (keys %old_hash) {
+        foreach my $prop (keys %{ $old_hash{$element} }) {
+            my $value = $old_hash{$element}->{$prop};
+            $new_hash{$prop}->{$element} = $value;
+        }
+    }
+
+    # say "new_hash:";
+    # use Data::Dumper;
+    # print Dumper(\%new_hash);
+
+    return wantarray ? %new_hash : \%new_hash;
 }

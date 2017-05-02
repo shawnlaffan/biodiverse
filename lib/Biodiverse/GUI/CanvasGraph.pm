dbcb9db01ad7a5e35033e73a64341d4af4dc8f26
Jason Mumbulla <jasonmumbulla@gmail.com>
2017-05-02 10:08:06 +1000

    Inital commit for the graph popup menu. Only implements clear graph.

--- lib/Biodiverse/GUI/CanvasGraph.pm
+++ lib/Biodiverse/GUI/CanvasGraph.pm
@@ -9,6 +9,7 @@ use Carp;
 use Gnome2::Canvas;
 
 use Gtk2;
+use Glib qw/TRUE FALSE/;
 
 use Scalar::Util qw(looks_like_number);
 use List::Util qw(min max);
@@ -25,8 +26,10 @@ use constant NUMBER_OF_AXIS_LABELS => 3;
 # gap from the bottom of the graph to the labels
 use constant X_AXIS_LABEL_PADDING => 5;
 # distance from the left of the graph to the start of the label
-use constant Y_AXIS_LABEL_PADDING => 80;
+use constant Y_AXIS_LABEL_PADDING => 30;
 use constant LABEL_FONT => 'Sans 9';
+use constant COLOUR_BLACK        => Gtk2::Gdk::Color->new(0, 0, 0);
+use constant COLOUR_GREY        => Gtk2::Gdk::Color->new(224, 224, 224);
 
 sub new {
     my $class = shift;
@@ -40,7 +43,7 @@ sub generate_canvas_graph {
     my ($self, %args) = @_;
     my %graph_values = %{$args{graph_values}};
     #my @graphs = $self->generate_fake_graph();
-    
+
     my $canvas       = $args{canvas};
     my $root         = $canvas->root;
     my $point_colour = $args{colour} // Gtk2::Gdk::Color->new(200, 200, 255);
@@ -58,6 +61,11 @@ sub generate_canvas_graph {
             delete $graph_values{$x};
         }
     }
+    #my $box1 = Gtk2::VBox->new(FALSE, 0);
+    #$canvas->add($box1);
+    #$box1->set_size_request ($canvas_width, $canvas_height);
+
+    $canvas->signal_connect (button_press_event => \&_do_popup_menu);
 
     if($clear_canvas) {
         # clear the canvas (there must be a better way to do this?)
@@ -101,6 +109,23 @@ sub generate_canvas_graph {
                                           outline_color => 'black');
     }
 
+    my $xlab = Gnome2::Canvas::Item->new (
+        $root,
+        'Gnome2::Canvas::Text',
+        x => 88, y => 225,
+        markup => "<b>Value</b>",
+        anchor => 'nw',
+        fill_color_gdk => COLOUR_BLACK,
+    );
+
+    my $ylab = Gnome2::Canvas::Item->new (
+        $root,
+        'Gnome2::Canvas::Text',
+        x => -80, y => 95,
+        markup => "<b>Key</b>",
+        anchor => 'nw',
+        fill_color_gdk => COLOUR_BLACK,
+    );
     # scale the values so they fit nicely in the canvas space.
     my %scaled_graph_values = $self->rescale_graph_points(
         old_values   => \%graph_values,
@@ -214,7 +239,7 @@ sub add_axis_labels_to_graph_canvas {
             + $min_x;
 
         # TODO Add format choice
-        $text = sprintf("%.g", $text);
+        $text = sprintf("%.2g", $text);
         my $x_label = Gnome2::Canvas::Item->new (
             $root, 'Gnome2::Canvas::Text',
             x => $x_position,
@@ -235,7 +260,7 @@ sub add_axis_labels_to_graph_canvas {
             + $min_y;
 
         # TODO Add format choice
-        $text = sprintf("%.2e", $text);
+        $text = sprintf("%.2g", $text);
         my $x_label = Gnome2::Canvas::Item->new (
             $root, 'Gnome2::Canvas::Text',
             x => - Y_AXIS_LABEL_PADDING,
@@ -243,6 +268,7 @@ sub add_axis_labels_to_graph_canvas {
             fill_color => 'black',
             font => LABEL_FONT,
             anchor => 'GTK_ANCHOR_NW',
+            justification =>  'right',
             text => $text,
             );
     }
@@ -372,4 +398,95 @@ sub generate_fake_graph {
     }
     
     return wantarray ? %graph : \%graph;
+}
+
+sub clear_graph {
+    my $self = shift;
+    my $canvas = shift;
+
+    my $root         = $canvas->root;
+    my ($canvas_width, $canvas_height) = (CANVAS_WIDTH, CANVAS_HEIGHT);
+
+    # Draw a white box over the whole canvas
+    my $box = Gnome2::Canvas::Item->new ($root, 'Gnome2::Canvas::Rect',
+                                      x1 => -$canvas_width,
+                                      y1 => -$canvas_height,
+                                      x2 => $canvas_width*2,
+                                      y2 => $canvas_height*2,
+                                      fill_color => 'white',
+                                      outline_color => 'white');
+
+    # Draw the black box that outlines the graph
+    my $box = Gnome2::Canvas::Item->new ($root, 'Gnome2::Canvas::Rect',
+                                     x1 => -(POINT_WIDTH),
+                                      y1 => -(POINT_HEIGHT),
+                                      x2 => $canvas_width+POINT_WIDTH,
+                                      y2 => $canvas_height+POINT_HEIGHT,
+                                      fill_color => 'white',
+                                      outline_color => 'black');
+    # add axis labels
+    my %zero_data = ( 1 => 0, 2 => 0, 3 => 0, 4 => 0, 5 => 0 );
+    #add_axis_labels_to_graph_canvas( graph_values => %zero_data,
+    #                                        canvas       => $canvas,
+    #                                        canvas_width => $canvas_width,
+    #                                        canvas_height => $canvas_height,
+    #                                       );
+}
+
+sub _do_popup_menu {
+    # Just clean the graph at the moment.
+    my ($self, $event) = @_;
+    if ($event->button != 3) {  # ignore other than button3
+        return 0;  # propagate event
+    }
+
+    # Create the menu items
+    my $menu = Gtk2::Menu->new;
+    my $logX_item = Gtk2::CheckMenuItem->new('_log X');
+    my $logY_item = Gtk2::CheckMenuItem->new('_log Y');
+    my $clear_item = Gtk2::MenuItem->new ("Clear");
+    my $sep_item = Gtk2::SeparatorMenuItem->new();
+    my $sep2_item = Gtk2::SeparatorMenuItem->new();
+
+
+    my $display_item = Gtk2::RadioMenuItem->new(undef,'Refresh with selection');
+    #connect to the toggled signal to catch the changes
+    $display_item->signal_connect('toggled' => \&toggle,"Refresh with selection");
+    my $group = $display_item->get_group;
+    my $display2_item = Gtk2::RadioMenuItem->new($group, 'Add selection');
+    #connect to the toggled signal to catch the changes
+    $display2_item->signal_connect('toggled' => \&toggle,"Add selection");
+
+
+    # Add them to the menu
+    $menu->append($display_item);
+    $menu->append($display2_item);
+    $menu->append($sep2_item);
+    $menu->append($logX_item);
+    $menu->append($logY_item);
+    $menu->append($sep_item);
+    $menu->append($clear_item);
+
+    # Attach the callback functions to the activate signal
+    $logX_item->signal_connect('toggled' => \&toggle,"log X");
+    $logY_item->signal_connect('toggled' => \&toggle,"log Y");
+    $clear_item->signal_connect( 'activate' =>  \&clear_graph, $self);
+
+    $display_item->show;
+    $display2_item->show;
+    $sep2_item->show;
+    $logX_item->show;
+    $logY_item->show;
+    $sep_item->show;
+    $clear_item->show;
+    $menu->popup (undef, undef, undef, undef, 0, 0);
+    return 0;
+}
+
+sub toggle {
+    my ($menu_item,$text) = @_;
+        my $val = $menu_item->get_active;
+        say "\$val: $val";
+        ($val)&&(print "$text active\n");
+        ($val)||(print "$text not active\n");
 }

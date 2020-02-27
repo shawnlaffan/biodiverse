c263e72f92c3e9bb33c946bf23b95703ffa9ee74
Shawn Laffan <shawnlaffan@gmail.com>
2020-02-27 16:29:29 +1100

    Use array args variant of function
    Minor speed up

--- lib/Biodiverse/Matrix.pm
+++ lib/Biodiverse/Matrix.pm
@@ -597,10 +597,7 @@ sub get_element_pairs_with_value {
 
     while ( my ( $el1, $hash_ref ) = each %$element_hash ) {
         foreach my $el2 ( keys %$hash_ref ) {
-            my $value = $self->get_defined_value(
-                element1 => $el1,
-                element2 => $el2
-            );
+            my $value = $self->get_defined_value_aa($el1, $el2);
             #  stringification implicitly uses %.15f precision
             next if $val ne $value;    
             $results{$el1}{$el2}++;

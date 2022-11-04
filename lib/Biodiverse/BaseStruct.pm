2c159d7feec4254f45175df70180bc08aed27ffa
shawnlaffan <shawnlaffan@gmail.com>
2022-11-04 20:54:08 +1100

    View labels: Avoid method call for sample redundancy
    Micro-optimisation but might save a few seconds
    of user experience with large data sets.

--- lib/Biodiverse/BaseStruct.pm
+++ lib/Biodiverse/BaseStruct.pm
@@ -1701,10 +1701,15 @@ sub get_base_stats {  #  calculate basestats for a single element
     my $element = $args{element}
       // croak "element not specified\n";
 
+    my $variety = $self->get_variety_aa      ($element);
+    my $samples = $self->get_sample_count_aa ($element);
+    #  avoid a method call with large data sets
+    my $redundancy = eval {1 - $variety / $samples};
+
     my %stats = (
-        VARIETY    => $self->get_variety_aa      ($element),
-        SAMPLES    => $self->get_sample_count_aa ($element),
-        REDUNDANCY => $self->get_redundancy_aa   ($element),
+        VARIETY    => $variety,
+        SAMPLES    => $samples,
+        REDUNDANCY => $redundancy,
     );
 
     #  get all the user defined properties

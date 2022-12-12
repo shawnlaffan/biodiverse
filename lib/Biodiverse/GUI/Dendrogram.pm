6b1b759ededc580e0d037313d5918231c5375f6f
shawnlaffan <shawnlaffan@gmail.com>
2022-12-12 13:59:16 +1100

    GUI: use correct reference
    Updates #841

--- lib/Biodiverse/GUI/Dendrogram.pm
+++ lib/Biodiverse/GUI/Dendrogram.pm
@@ -1232,7 +1232,7 @@ sub recolour_cluster_lines {
     my $colour_mode  = $self->get_cluster_colour_mode();
     
     my $is_canape = $list_name =~ />>CANAPE>>/ && $list_index =~ /^CANAPE/;
-    my $is_zscore = $self->{parent}->index_is_zscore (list => $list_name, index => $list_index);
+    my $is_zscore = $self->{parent_tab}->index_is_zscore (list => $list_name, index => $list_index);
 
     foreach my $node_ref (@$cluster_nodes) {
 

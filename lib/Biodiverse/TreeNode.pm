f9bcf98d18da19169c0847e8fa99f9e2d0cee220
shawnlaffan <shawnlaffan@gmail.com>
2024-02-23 12:13:09 +1100

    TreeNode.pm: use a linear scan for get_hash_lists_below
    Might as well avoid any recursion overheads.

--- lib/Biodiverse/TreeNode.pm
+++ lib/Biodiverse/TreeNode.pm
@@ -1559,15 +1559,12 @@ sub get_shared_ancestor {
 #  get the list of hashes in the nodes
 sub get_hash_lists {
     my $self = shift;
-    my %args = @_;
-    
-    my @list;
-    foreach my $tmp (keys %{$self}) {
-        next if $tmp =~ /^_/;  #  skip the internals
-        push @list, $tmp if is_hashref($self->{$tmp});
-    }
-    return @list if wantarray;
-    return \@list;
+
+    my @list
+      = grep {$_ !~ /^_/ and is_hashref $self->{$_}}
+        keys %$self;
+
+    return wantarray ? @list : \@list;
 }
 
 sub get_hash_lists_below {
@@ -1577,9 +1574,11 @@ sub get_hash_lists_below {
     my %hash_list;
     @hash_list{@list} = undef;
 
-    foreach my $child ($self->get_children) {
-        my $list_below = $child->get_hash_lists_below;
+    my @children = $self->get_children;
+    while (my $child = shift @children) {
+        my $list_below = $child->get_hash_lists;
         @hash_list{@$list_below} = undef;
+        push @children, $child->get_children;
     }
     
     return wantarray

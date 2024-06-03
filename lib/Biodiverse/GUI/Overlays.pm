18b3413d7d74f12c4ceec9455b55a766251e373e
shawnlaffan <shawnlaffan@gmail.com>
2024-06-04 09:40:20 +1000

    GUI overlays: attempt at per-layer colours, currently disabled

--- lib/Biodiverse/GUI/Overlays.pm
+++ lib/Biodiverse/GUI/Overlays.pm
@@ -16,9 +16,10 @@ use Biodiverse::GUI::Project;
 my $default_colour       = Gtk2::Gdk::Color->parse('#001169');
 my $last_selected_colour = $default_colour;
 
-use constant COL_FNAME     => 0;
-use constant COL_PLOT_POLY => 1;
-# use constant COL_COLOUR    => 2;
+use constant COL_FNAME       => 0;
+use constant COL_PLOT_POLY   => 1;
+use constant COL_PLOT_COLOUR => 2;
+use constant COL_PLOT_COLOUR_BK => 3;
 
 sub show_dialog {
     my $grid = shift;
@@ -95,7 +96,7 @@ sub init_overlay_list {
     #$name_renderer->signal_connect('toggled' => \&_update_colour_for_selection, $model);
     $col_name->set_title('File name');
     $col_name->pack_start($name_renderer, 1);
-    $col_name->add_attribute($name_renderer,  text => 0);
+    $col_name->add_attribute($name_renderer,  text => COL_FNAME);
     $tree->insert_column($col_name, -1);
 
     my $col_plot_poly = Gtk2::TreeViewColumn->new();
@@ -108,13 +109,24 @@ sub init_overlay_list {
     );
     $tree->insert_column($col_plot_poly, -1);
 
-    #  fiddling around with colour selection
-    #my $colColour = Gtk2::TreeViewColumn->new();
-    #my $colour_button = Gtk2::CellRendererPixbuf->new();
-    #$colColour->set_title('Colour');
-    #$colColour->pack_start($colour_button, 1);
-    #$colColour->add_attribute($colour_button, text => 0);
-    #$tree->insert_column($colColour, -1);
+    # my $col_colour = Gtk2::TreeViewColumn->new();
+    # my $colour_renderer_toggle = Gtk2::CellRendererToggle->new();
+    # my $colour_renderer_text   = Gtk2::CellRendererText->new();
+    # $col_colour->set_title('Colour');
+    # $col_colour->pack_start($colour_renderer_toggle, 0);
+    # $col_colour->pack_start($colour_renderer_text, 0);
+    # $col_colour->add_attribute($colour_renderer_toggle, active => COL_PLOT_COLOUR);
+    # $col_colour->set_attributes($colour_renderer_toggle, active => 1);
+    # $col_colour->add_attribute($colour_renderer_text,
+    #     'cell-background' => COL_PLOT_COLOUR_BK,
+    # );
+    # $col_colour->set_attributes($colour_renderer_text,
+    #     'cell-background' => COL_PLOT_COLOUR_BK,
+    # );
+    # $colour_renderer_toggle->signal_connect(
+    #     'toggled' => \&_update_colour_for_selection, $model
+    # );
+    # $tree->insert_column($col_colour, -1);
 
     $tree->set_headers_visible(1);
     $tree->set_model($model);
@@ -135,18 +147,45 @@ sub _plot_poly_toggled {
     $model->set ($iter, COL_PLOT_POLY, !$bool);
 }
 
-# sub _update_colour_for_selection {
-#     my ($cell, $path_str, $model) = @_;
-#
-#     my $path = Gtk2::TreePath->new_from_string ($path_str);
-#
-#     # get toggled iter
-#     my $iter = $model->get_iter ($path);
-#     my $colour = $model->get ($iter, COL_PLOT_COLOUR);
-#
-#     # toggle the value
-#     $model->set ($iter, COL_PLOT_POLY, !$bool);
-# }
+sub _update_colour_for_selection {
+    my ($cell, $path_str, $model) = @_;
+
+    my $path = Gtk2::TreePath->new_from_string ($path_str);
+
+    # get toggled iter
+    my $iter = $model->get_iter ($path);
+    my ($bool) = $model->get ($iter, COL_PLOT_COLOUR);
+
+    my $gui = Biodiverse::GUI::GUIManager->instance;
+    my $overlay_components = $gui->get_overlay_components;
+    my $colour_button = $overlay_components->{colour_button};
+
+    my $colour = $model->get ($iter, COL_PLOT_COLOUR_BK);
+    if (!defined $colour or $colour eq 'undef') {
+        $colour = $colour_button->get_colour; #$self->get_last_colour;
+    }
+
+    if (!Scalar::Util::blessed $colour) {
+        $colour = Gtk2::Gdk::Color->parse($colour);
+    }
+    $colour_button->set_color($colour);
+
+    $colour_button->clicked;
+    $colour = $colour_button->get_color;
+
+    say STDERR $bool, ' ', $colour->to_string;
+
+    # toggle the value
+    $model->set ($iter,
+        COL_PLOT_COLOUR, !$bool,
+    );
+    $model->set ($iter,
+        COL_PLOT_COLOUR_BK, $colour->to_string,
+        # 'cell-background' => $colour->to_string,
+    );
+    say '--- ' .  $model->get ($iter, COL_PLOT_COLOUR);
+    say '--- ' .  $model->get ($iter, COL_PLOT_COLOUR_BK);
+}
 
 # Make the object tree that appears on the left
 sub make_overlay_model {
@@ -155,6 +194,8 @@ sub make_overlay_model {
     my $model = Gtk2::ListStore->new(
         'Glib::String',
         'Glib::Boolean',
+        # 'Glib::Boolean',  #  next two are colour stuff
+        # 'Glib::String',
     );
 
     my $overlays = $project->get_overlay_list();
@@ -165,12 +206,18 @@ sub make_overlay_model {
             $entry = {
                 name         => $entry,
                 plot_as_poly => 0,
+                # has_colour   => undef,
+                # colour       => undef,
             };
         }
+        # use DDP;
+        # p $entry;
         $model->set(
             $iter,
-            COL_FNAME,     $entry->{name},
-            COL_PLOT_POLY, $entry->{plot_as_poly}
+            COL_FNAME,       $entry->{name},
+            COL_PLOT_POLY,   $entry->{plot_as_poly},
+            # COL_PLOT_COLOUR, !!$entry->{colour},
+            # COL_PLOT_COLOUR_BK, ($entry->{colour} // 'white'),
         );
     }
 

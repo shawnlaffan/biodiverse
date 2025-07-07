90352705117b01f62ca142aacee4c8c04f8a7c23
shawnlaffan <shawnlaffan@gmail.com>
2025-07-07 18:45:39 +1000

    Matrix canvas: better use of refs to trigger caching
    For example, the Acacia data have only 176 colours
    across 961 cells.

--- lib/Biodiverse/GUI/Canvas/Matrix.pm
+++ lib/Biodiverse/GUI/Canvas/Matrix.pm
@@ -230,7 +230,7 @@ sub draw_cells_cb {
 sub recolour {
     my ($self) = @_;
 
-    state @default_colour = ((0.8) x 3);
+    state $default_colour = [(0.8) x 3];
 
     my $mx = $self->get_current_matrix;
     return if !$mx;
@@ -249,7 +249,8 @@ sub recolour {
     my $highlight_cols = keys %col_highlights;
     my $do_h = $highlight_rows && $highlight_cols;
 
-    state %colour_cache;
+    state %rgb_cache;
+    state %rgba_cache;
 
     my $x = -1;
     for my $col_label (@$col_labels) {
@@ -260,17 +261,17 @@ sub recolour {
         for my $row_label (@$row_labels) {
             $y++;
             my $val = $mx->get_defined_value_aa($col_label, $row_label);
-            my $colour = defined $val
-                ? $colour_cache{sprintf "%.4g", $val} //= do {
+            \my @colour = defined $val
+                ? $rgb_cache{sprintf "%.4g", $val} //= do {
                     my $c = $legend->get_colour($val);
                     [$c->red, $c->green, $c->blue]
                 }
-                : \@default_colour;
+                : $default_colour;
             my $alpha
                 = $do_h
                 ? ($highlight_col && $row_highlights{$row_label}) ? 1 : 0.4
                 : 1;
-            $data{"$x:$y"}->{rgba} = [@$colour, $alpha];
+            $data{"$x:$y"}->{rgba} = $rgba_cache{$alpha}{join ':', @colour} //= [@colour, $alpha];
         }
     }
 

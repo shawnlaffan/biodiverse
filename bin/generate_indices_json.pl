5ecbb3abb47018135bf010d562670768c733b22d
Shawn Laffan <shawnlaffan@gmail.com>
2025-11-04 16:24:03 +1100

    generate_indices_json.pl: write to file

--- bin/generate_indices_json.pl
+++ bin/generate_indices_json.pl
@@ -4,7 +4,12 @@ use 5.010;
 
 use rlib;
 
-use Biodiverse::Indices 
+use Biodiverse::Indices;
 
-say Biodiverse::Indices->get_calculation_metadata_as_json;
+my $file = ($ARGV[0] // 'indices.json');
 
+say "Exporting to $file";
+open my $fh, '>', $file or die $!;
+my $json = Biodiverse::Indices->get_calculation_metadata_as_json;
+say {$fh} $json;
+$fh->close;

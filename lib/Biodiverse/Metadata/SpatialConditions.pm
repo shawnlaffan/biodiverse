a6e21610b78efd2c4673e41938fa3ed79b720a17
Shawn Laffan <shawnlaffan@gmail.com>
2026-02-19 12:18:07 +1100

    spatial conditions metadata: is_def_query flag

--- lib/Biodiverse/Metadata/SpatialConditions.pm
+++ lib/Biodiverse/Metadata/SpatialConditions.pm
@@ -8,22 +8,23 @@ our $VERSION = '5.0';
 use parent qw /Biodiverse::Metadata/;
 
 my %methods_and_defaults = (
-    description    => 'no_description',
-    result_type    => 'no_type',
-    index_max_dist => undef,
-    shape_type     => 'unknown',
-    example        => 'no_example',
-    required_args  => [],
-    optional_args  => [],
-    index_no_use   => undef,
+    description            => 'no_description',
+    result_type            => 'no_type',
+    index_max_dist         => undef,
+    shape_type             => 'unknown',
+    example                => 'no_example',
+    required_args          => [],
+    optional_args          => [],
+    index_no_use           => undef,
     use_euc_distance       => undef,
     use_euc_distances      => [],
     use_abs_euc_distances  => [],
     use_cell_distance      => undef,
     use_cell_distances     => [],
     use_abs_cell_distances => [],
-    is_volatile_cb         => sub{},
+    is_volatile_cb         => sub {},
     requires_tree_ref      => undef,
+    is_def_query           => undef,
 );
 
 sub _get_method_default_hash {

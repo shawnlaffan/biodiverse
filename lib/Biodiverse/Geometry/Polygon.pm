a4eb2fad4a56a1263ba0bfab156b99cedbe50d85
Shawn Laffan <shawnlaffan@gmail.com>
2026-03-04 11:33:31 +1100

    Geometry/Polygon.pm: several updates and fixes
    Fix:
    - centroid calculation
    - shift method
    
    Add methods:
    - as_wkt
    - as_gdal_geometry

--- lib/Biodiverse/Geometry/Polygon.pm
+++ lib/Biodiverse/Geometry/Polygon.pm
@@ -4,6 +4,8 @@ use warnings;
 use 5.036;
 use Carp qw /croak/;
 
+use experimental /for_list declared_refs refaliasing/;
+
 our $VERSION = '5.0';
 
 sub new {
@@ -45,9 +47,13 @@ sub centre {
     return $self->{centre} if $self->{centre};
     my $e = $self->get_extent;
     return $self->{centre} = [
-        $e->[0] + $e->[2] / 2,
-        $e->[1] + $e->[3] / 2
+        ($e->[0] + $e->[2]) / 2,
+        ($e->[1] + $e->[3]) / 2
     ];
+}
+
+sub get_centroid {
+    $_[0]->centre;
 }
 
 sub xmin {
@@ -71,16 +77,50 @@ sub clone {
 
 sub shift {
     my ($self, $xoff, $yoff) = @_;
+    use experimental qw /declared_refs/;
+    use Ref::Util qw /is_arrayref/;
     my $clone = $self->clone;
     my $g = $clone->get_geometry;
-    foreach my \@part (@$g) {
-        foreach my \@vertices (@part) {
-            $vertices[0] += $xoff;
-            $vertices[1] += $yoff;
+    my @queue = $g;
+    ITEM:
+    while (my $item = shift @queue) {
+        if (is_arrayref $item->[0]) {
+            push @queue, @$item;
+            next ITEM;
         }
+        $item->[0] += $xoff;
+        $item->[1] += $yoff;
     }
+    $clone->{centre} = undef;
+    my $e = $clone->get_extent;
+    $e->[0] += $xoff;
+    $e->[1] += $yoff;
+    $e->[2] += $xoff;
+    $e->[3] += $yoff;
     $clone;
 }
 
+sub as_gdal_geometry {
+    my ($self) = @_;
+    Geo::GDAL::FFI::Geometry->new(WKT => $self->as_wkt);
+}
+
+sub as_wkt {
+    my ($self) = @_;
+    my $geom = $self->get_geometry;
+    my $wkt = 'MULTIPOLYGON (';
+    foreach my $poly (@$geom) {
+        foreach my $part (@$poly) {
+            my $wkt_part = '((';
+            my @vertices = map {"$_->[0] $_->[1]"} @$part;
+            $wkt_part .= join ', ', @vertices;
+            $wkt_part .= ')), ';
+            $wkt .= $wkt_part;
+        }
+        $wkt =~ s/,\s*$//;
+    }
+    $wkt .= ')';
+    # say STDERR $wkt;
+}
 
 1;

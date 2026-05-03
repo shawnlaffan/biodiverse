23b8f610938da9f9a57a1af62b3babaacf134306
Shawn Laffan <shawnlaffan@gmail.com>
2026-05-03 19:45:48 +1000

    raster imports: use ndarray size for progress

--- lib/Biodiverse/BaseData/Import.pm
+++ lib/Biodiverse/BaseData/Import.pm
@@ -940,7 +940,7 @@ sub import_data_raster {
                             $gp_lb_hash{$gp}{$this_label} += $arr[2];
                         }
 
-                        $processed_count += ($maxw - $wpos) * ($maxh - $hpos);
+                        $processed_count += $z->nelem;
 
                         $wpos += $blockw;
                         next TILE;

65cd218f90cc78820e69f1c44dbd81b1121c49ef
Shawn Laffan <shawnlaffan@gmail.com>
2026-06-02 09:38:28 +1000

    indices tests: generate JSON data

--- t/lib/Biodiverse/TestHelpers.pm
+++ t/lib/Biodiverse/TestHelpers.pm
@@ -1152,42 +1152,13 @@ sub print_indices_result_set_to_fh {
 
     return if !$fh;
 
-    require Perl::Tidy;
-    use Data::Dumper;
-
-    local $Data::Dumper::Purity    = 1;
-    local $Data::Dumper::Terse     = 1;
-    local $Data::Dumper::Sortkeys  = 1;
-    local $Data::Dumper::Indent    = 1;
-    local $Data::Dumper::Quotekeys = 0;
-    #say '#' x 20;
-
-    my $source_string = Dumper($results_hash);
-    my $dest_string;
-    my $stderr_string;
-    my $errorfile_string;
-    my $argv = "-npro";   # Ignore any .perltidyrc at this site
-    $argv .= " -pbp";     # Format according to perl best practices
-    $argv .= " -nst";     # Must turn off -st in case -pbp is specified
-    $argv .= " -se";      # -se appends the errorfile to stderr
-    #$argv .= " -no-log";  # Don't write the log file - disable as it seems to cause issues these days
-
-    my $error = Perl::Tidy::perltidy(
-        argv        => $argv,
-        source      => \$source_string,
-        destination => \$dest_string,
-        stderr      => \$stderr_string,
-        errorfile   => \$errorfile_string,    # ignored when -se flag is set
-        ##phasers   => 'stun',                # uncomment to trigger an error
-    );
-
-    warn 'PERLTIDY RETURNED ERROR'
-      if $error;
+    use JSON::MaybeXS;
+    my $json = JSON->new->canonical->ascii->pretty;
+    my $dest_string = $json->encode ($results_hash);
 
     say   {$fh} "@@ RESULTS_${nbr_list_count}_NBR_LISTS";
     say   {$fh} $dest_string;
     print {$fh} "\n";
-    #say '#' x 20;
 
     return;   
 }
